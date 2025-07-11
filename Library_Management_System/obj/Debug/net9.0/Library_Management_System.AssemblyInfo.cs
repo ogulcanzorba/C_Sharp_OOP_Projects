e8ba6ae6d77dce8c913f1c0e9c9688661b0823c2
Olci <ogulcan1134@gmail.com>
2025-07-11 15:49:34 +0300

    .gitignore file's location has been changed.

--- Library_Management_System/obj/Debug/net9.0/Library_Management_System.AssemblyInfo.cs
+++ Library_Management_System/obj/Debug/net9.0/Library_Management_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e591508ff5f476c08508f9f5ecb8c7273ff5e2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa9f4e72aa7a55bbd2a5da9f1f47752518b6ca5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
